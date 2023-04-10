4184167404473c111b1f0aaaaef41072e9df2b86
myungbin <kki9621@naver.com>
2023-04-10 14:44:18 +0900

    Init

---
+++ src/models/predict_model.py
@@ -0,0 +1,158 @@
+import torch
+import torch.nn as nn
+
+
+class LSTMAutoencoder(nn.Module):
+    def __init__(self, input_dim, hidden_dim, num_layers, dropout):
+        super().__init__()
+        self.input_dim = input_dim
+        self.hidden_dim = hidden_dim
+        self.num_layers = num_layers
+        self.dropout = dropout
+
+        self.encoder = nn.LSTM(
+            input_size=input_dim,
+            hidden_size=hidden_dim,
+            num_layers=num_layers,
+            dropout=dropout,
+            batch_first=True
+        )
+
+        self.decoder = nn.LSTM(
+            input_size=hidden_dim,
+            hidden_size=input_dim,
+            num_layers=num_layers,
+            dropout=dropout,
+            batch_first=True
+        )
+        self.fc = nn.Linear(hidden_dim, hidden_dim)
+
+    def forward(self, x):
+        encoded, _ = self.encoder(x)
+        latent = self.fc(encoded)
+        decoded, _ = self.decoder(latent)
+        return decoded
+
+
+class LSTMAutoencoder4(nn.Module):
+    def __init__(self, input_size, hidden_size, num_layers=1):
+        super(LSTMAutoencoder4, self).__init__()
+
+        self.encoder = nn.LSTM(input_size, hidden_size, num_layers, batch_first=True)
+        self.decoder = nn.LSTM(hidden_size, input_size, num_layers, batch_first=True)
+
+    def forward(self, x):
+        # Encoding
+        _, (hidden_state, cell_state) = self.encoder(x)
+
+        # Prepare the initial hidden state for the decoder
+        decoder_input = torch.zeros_like(x)
+        decoder_hidden = (hidden_state, cell_state)
+
+        # Decoding
+        decoded, _ = self.decoder(decoder_input, decoder_hidden)
+
+        return decoded
+
+
+class AutoEncoder(nn.Module):
+    def __init__(self, input_dim, latent_dim=32):
+        super(AutoEncoder, self).__init__()
+
+        self.Encoder = nn.Sequential(
+            nn.Linear(input_dim, 256),
+            nn.BatchNorm1d(256),
+            nn.LeakyReLU(),
+            nn.Linear(256, 128),
+            nn.BatchNorm1d(128),
+            nn.LeakyReLU(),
+            nn.Linear(128, 64),
+            nn.BatchNorm1d(64),
+            nn.LeakyReLU(),
+            nn.Linear(64, latent_dim),
+        )
+        self.Decoder = nn.Sequential(
+            nn.Linear(latent_dim, 64),
+            nn.BatchNorm1d(64),
+            nn.LeakyReLU(),
+            nn.Linear(64, 128),
+            nn.BatchNorm1d(128),
+            nn.LeakyReLU(),
+            nn.Linear(128, 256),
+            nn.BatchNorm1d(256),
+            nn.LeakyReLU(),
+            nn.Linear(256, input_dim)
+        )
+
+    def forward(self, x):
+        x = self.Encoder(x)
+        x = self.Decoder(x)
+        return x
+
+
+class Encoder(nn.Module):
+    def __init__(self, seq_len, n_features, embedding_dim=64):
+        super(Encoder, self).__init__()
+        self.seq_len, self.n_features = seq_len, n_features
+        self.embedding_dim, self.hidden_dim = embedding_dim, 2 * embedding_dim
+        self.rnn1 = nn.LSTM(
+            input_size=n_features,
+            hidden_size=self.hidden_dim,
+            num_layers=1,
+            batch_first=True
+        )
+        self.rnn2 = nn.LSTM(
+            input_size=self.hidden_dim,
+            hidden_size=embedding_dim,
+            num_layers=1,
+            batch_first=True
+        )
+
+    def forward(self, x):
+        x = x.reshape((1, self.seq_len, self.n_features))
+        x, (_, _) = self.rnn1(x)
+        x, (hidden_n, _) = self.rnn2(x)
+        return hidden_n.reshape((self.n_features, self.embedding_dim))
+
+
+class Decoder(nn.Module):
+    def __init__(self, seq_len, input_dim=64, n_features=1):
+        super(Decoder, self).__init__()
+        self.seq_len, self.input_dim = seq_len, input_dim
+        self.hidden_dim, self.n_features = 2 * input_dim, n_features
+        self.rnn1 = nn.LSTM(
+            input_size=input_dim,
+            hidden_size=input_dim,
+            num_layers=1,
+            batch_first=True
+        )
+        self.rnn2 = nn.LSTM(
+            input_size=input_dim,
+            hidden_size=self.hidden_dim,
+            num_layers=1,
+            batch_first=True
+        )
+        self.output_layer = nn.Linear(self.hidden_dim, n_features)
+
+    def forward(self, x):
+        x = x.repeat(self.seq_len, self.n_features)
+        x = x.reshape((self.n_features, self.seq_len, self.input_dim))
+        x, (hidden_n, cell_n) = self.rnn1(x)
+        x, (hidden_n, cell_n) = self.rnn2(x)
+        x = x.reshape((self.seq_len, self.hidden_dim))
+        return self.output_layer(x)
+
+
+class RecurrentAutoencoder(nn.Module):
+    def __init__(self, seq_len, n_features, embedding_dim=64):
+        super(RecurrentAutoencoder, self).__init__()
+        self.encoder = Encoder(seq_len, n_features, embedding_dim).to('cuda')
+        self.decoder = Decoder(seq_len, embedding_dim, n_features).to('cuda')
+
+    def forward(self, x):
+        x = self.encoder(x)
+        x = self.decoder(x)
+        return x
+
+
+
---
+++ src/train/train.py
@@ -0,0 +1,57 @@
+from datetime import datetime
+
+import torch
+import torch.nn as nn
+import numpy as np
+import pandas as pd
+
+from src.config.config import cfg
+
+
+def train(train_loader, model, criterion, optimizer):
+    model.train()
+    model.to(cfg.DEVICE)
+    for epoch in range(cfg.EPOCHS):
+        for data in train_loader:
+            data = data.to(cfg.DEVICE)
+            optimizer.zero_grad()
+
+            output = model(data)
+            loss = criterion(output, data)
+
+            loss.backward()
+            optimizer.step()
+
+        print(f'Epoch [{epoch + 1}/{cfg.EPOCHS}], Loss: {loss.item():.7f}')
+
+
+def evaluation(test_loader, model):
+    model.eval()
+    model.to("cpu")
+    pred = []
+    threshold = []
+    with torch.no_grad():
+        for data in iter(test_loader):
+            data = data.to("cpu")
+            prediction = model(data)
+
+            cos = nn.CosineSimilarity(dim=1)
+            cosine = cos(data, prediction).tolist()
+            batch_pred = np.where(np.array(cosine) < 0.99, 1, 0).tolist()
+
+            # mse = np.mean(np.power(data.detach().numpy() - prediction.detach().numpy(), 2), axis=1)
+            # threshold = np.mean(mse) + 3 * np.std(mse)
+            # batch_pred = np.where(np.array(mse) < threshold, 0, 1).tolist()
+
+            threshold += cosine
+            pred += batch_pred
+    return pred, threshold
+
+
+def prediction_to_csv(prediction):
+    submission = pd.read_csv(cfg.SUBMISSION_PATH)
+    submission["label"] = prediction
+    print(submission.label.value_counts())
+    current_time = datetime.now().strftime('%Y%m%d_%H%M%S')
+    submission.to_csv(
+        f'data/submission/{current_time}submission.csv', index=False)
